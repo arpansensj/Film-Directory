dc6391e56f78a6de82b31418f9635d63849506d5
Arpan Sen <sen.arpan12@gmail.com>
2024-02-12 02:23:15 +0530

    Minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <div>
-      <h1 class="font-bold text-5xl py-2 text-purple-500 shadow-md text-center">FILM DIRECTORY</h1>
+      <h1 class="font-bold text-5xl py-2 text-yellow-500 shadow-md text-center">FILM DIRECTORY</h1>
 
       <div className='py-2 '>
         <Card/>
